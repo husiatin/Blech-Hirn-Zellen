f9eb9df3ebfd0ce2c7170d5d9a7b013cfa800d49
Dustin Lojewski <dustin.lojewski@gmail.com>
2025-11-09 17:51:43 +0100

    feat(backend): Rework the structure of the files, add comments and write the README file

--- backend/fastapi/app/main.py
+++ backend/fastapi/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(redoc_url=None)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "ROOT!"}
+
+@app.get("/app")
+def read_app():
+    return {"Hello": "APP"}
